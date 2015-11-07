34024bb97f4b731f9a2ad80e88291e8322853ef0
Robert Weindl <robert.weindl@sinus.io>
2015-11-07 21:15:24 +0800

    Implementation of ng-enter directive.

--- web/public/modules/search/controllers/search.client.controller.js
+++ web/public/modules/search/controllers/search.client.controller.js
@@ -10,10 +10,20 @@ angular.module('search').controller('SearchController', ['$scope','$http', '$loc
 
     // TODO: REST Call to receive search results dynamically.
     $scope.search = function(query) {
+      if ($scope.preventNextSearch) {
+				return [];
+			}
+
       var resultList = ['BMW', 'Volkswagen'];
       $scope.searchResults = resultList.filter(function(searchResult) {
         return searchResult.toLowerCase().indexOf(query.toLowerCase()) > -1
       });
+    };
+
+    $scope.select = function(searchResult) {
+				$scope.searchValue = searchResult;
+        alert(searchResult)
+        // TODO: Change route here.
     };
   }
 ]);
