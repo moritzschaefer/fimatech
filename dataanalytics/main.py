2c6da6a79f45b83cbb18bb0927121ebf89254296
Moritz Schäfer <mail@moritzs.de>
2015-11-07 20:40:56 +0800

    add boilerplate for dataanalyicts lib

---
+++ dataanalytics/main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def process_company(company, stock_data, articles):
+    """ Processes the articles for given company and returns a list of newspapers with according calculated values. check README.md for more information
+
+    :company: name of the company to be used
+    :stock_data: stock_data for given company.
+    :articles: list of articles with attributes date, newspaper, ...
+    :returns: list of newspapers with according values for max, best and worst impact
+
+    """
+
+    pass
+
